a73a062403f9635e0a8c7583080db889dd6102ff
victor_shulpov <shulpovvitya01@mail.ru>
2020-07-10 23:47:16 +0700

    данные выводятся в dataGrid в форме запросов, но пока без условий

--- Pharmacy_DB/RequestForm.cpp
+++ Pharmacy_DB/RequestForm.cpp
@@ -1,2 +1,118 @@
 #include "RequestForm.h"
+#include "Functions.h"
+
+
+
+
+void PharmacyDB::RequestForm::Header()
+{
+    DataGridViewTextBoxColumn^ c1 = gcnew DataGridViewTextBoxColumn();
+    c1->Name = "Id";
+    c1->HeaderText = "id";
+    c1->Width = 150;
+    dataGridViewRequest->Columns->Add(c1);
+
+    DataGridViewTextBoxColumn^ c2 = gcnew DataGridViewTextBoxColumn();
+    c2->Name = "Name";
+    c2->HeaderText = "Название";
+    c2->Width = 450;
+    dataGridViewRequest->Columns->Add(c2);
+
+    DataGridViewTextBoxColumn^ c3 = gcnew DataGridViewTextBoxColumn();
+    c3->Name = "Country";
+    c3->HeaderText = "Страна-производитель";
+    c3->Width = 150;
+    dataGridViewRequest->Columns->Add(c3);
+    DataGridViewTextBoxColumn^ c4 = gcnew DataGridViewTextBoxColumn();
+    c4->Name = "Date";
+    c4->HeaderText = "Дата производства";
+    c4->Width = 150;
+    dataGridViewRequest->Columns->Add(c4);
+
+    DataGridViewTextBoxColumn^ c5 = gcnew DataGridViewTextBoxColumn();
+    c5->Name = "PharmacyNumbers";
+    c5->HeaderText = "аптека 1";
+    c5->Width = 50;
+    dataGridViewRequest->Columns->Add(c5);
+
+    DataGridViewTextBoxColumn^ c6 = gcnew DataGridViewTextBoxColumn();
+    c6->Name = "PharmacyNumbers";
+    c6->HeaderText = "аптека 2";
+    c6->Width = 50;
+    dataGridViewRequest->Columns->Add(c6);
+
+    DataGridViewTextBoxColumn^ c7 = gcnew DataGridViewTextBoxColumn();
+    c7->Name = "PharmacyNumbers";
+    c7->HeaderText = "аптека 3";
+    c7->Width = 50;
+    dataGridViewRequest->Columns->Add(c7);
+
+    DataGridViewTextBoxColumn^ c8 = gcnew DataGridViewTextBoxColumn();
+    c8->Name = "PharmacyNumbers";
+    c8->HeaderText = "аптека 4";
+    c8->Width = 50;
+    dataGridViewRequest->Columns->Add(c8);
+
+    DataGridViewTextBoxColumn^ c9 = gcnew DataGridViewTextBoxColumn();
+    c9->Name = "PharmacyNumbers";
+    c9->HeaderText = "аптека 5";
+    c9->Width = 50;
+    dataGridViewRequest->Columns->Add(c9);
+
+    DataGridViewTextBoxColumn^ c10 = gcnew DataGridViewTextBoxColumn();
+    c10->Name = "Price";
+    c10->HeaderText = "Цена";
+    c10->Width = 150;
+    dataGridViewRequest->Columns->Add(c10);
+
+    dataGridViewRequest->AutoResizeColumns();
+    dataGridViewRequest->TopLeftHeaderCell->Value = "Данные по запросу";
+}
+void PharmacyDB::RequestForm::Show_meds(int _countData)
+{
+    int countData = 0;//текущее кол-во лекарств
+    char fileName[] = "medicines.txt"; //основной файл
+    if (_countData == 0)
+    {
+        MessageBox::Show("Записей в файле нет", "Предупреждение");
+    }
+    else {
+        dataGridViewRequest->RowCount = _countData + 1;
+        dataGridViewRequest->ColumnCount = 10;
+        std::string line;
+        std::ifstream in(fileName); // окрываем файл для чтения
+        int cell_num = 0, count_row = 0;
+        if (in.is_open())
+        {
+            getline(in, line);
+            while (getline(in, line, ';'))
+            {
+                if (cell_num > 9) {
+                    cell_num = 0;
+                    count_row++;
+                }
+                dataGridViewRequest->Rows[count_row]->Cells[cell_num]->Value = Convert_string_to_String(line);
+                cell_num++;
+            }
+        }
+        else
+            MessageBox::Show("Файл не открылся", "Ошибка");//Файл не открылся 
+        in.close();     // закрываем файл
+    }
+    return;
+}
+
+System::Void PharmacyDB::RequestForm::RequestForm_Shown(System::Object^ sender, System::EventArgs^ e)
+{
+    char fileName[] = "medicines.txt"; //основной файл
+
+
+    int countData = get_count_data(fileName);//текущее кол-во лекарств
+
+   
+    Header();
+    MessageBox::Show(Convert_string_to_String(std::to_string(countData)));
+    Show_meds(countData);
+    return System::Void();
+}
 
--- Pharmacy_DB/RequestForm.h
+++ Pharmacy_DB/RequestForm.h
@@ -35,8 +35,9 @@ namespace PharmacyDB {
 			}
 		}
 	public: System::Windows::Forms::GroupBox^ groupBox1;
+	public: System::Windows::Forms::DataGridView^ dataGridViewRequest;
 	protected:
-	public: System::Windows::Forms::DataGridView^ dataGridViewAdmin;
+
 	private: System::Windows::Forms::MenuStrip^ menuStrip1;
 	public:
 	private: System::Windows::Forms::ToolStripMenuItem^ âûõîäToolStripMenuItem;
@@ -68,7 +69,7 @@ namespace PharmacyDB {
 		void InitializeComponent(void)
 		{
 			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
-			this->dataGridViewAdmin = (gcnew System::Windows::Forms::DataGridView());
+			this->dataGridViewRequest = (gcnew System::Windows::Forms::DataGridView());
 			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
 			this->âûõîäToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
 			this->âûéòèÈçÏğîãğàììûToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
@@ -85,35 +86,38 @@ namespace PharmacyDB {
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
 			this->groupBox1->SuspendLayout();
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewAdmin))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewRequest))->BeginInit();
 			this->menuStrip1->SuspendLayout();
 			this->groupBox2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// groupBox1
 			// 
-			this->groupBox1->Controls->Add(this->dataGridViewAdmin);
-			this->groupBox1->Location = System::Drawing::Point(12, 35);
+			this->groupBox1->Controls->Add(this->dataGridViewRequest);
+			this->groupBox1->Location = System::Drawing::Point(9, 28);
+			this->groupBox1->Margin = System::Windows::Forms::Padding(2);
 			this->groupBox1->Name = L"groupBox1";
-			this->groupBox1->Size = System::Drawing::Size(1119, 642);
+			this->groupBox1->Padding = System::Windows::Forms::Padding(2);
+			this->groupBox1->Size = System::Drawing::Size(916, 522);
 			this->groupBox1->TabIndex = 3;
 			this->groupBox1->TabStop = false;
 			this->groupBox1->Text = L"Äàííûå";
 			// 
-			// dataGridViewAdmin
+			// dataGridViewRequest
 			// 
-			this->dataGridViewAdmin->AllowUserToAddRows = false;
-			this->dataGridViewAdmin->AllowUserToDeleteRows = false;
-			this->dataGridViewAdmin->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
-			this->dataGridViewAdmin->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
-			this->dataGridViewAdmin->Location = System::Drawing::Point(6, 21);
-			this->dataGridViewAdmin->Name = L"dataGridViewAdmin";
-			this->dataGridViewAdmin->ReadOnly = true;
-			this->dataGridViewAdmin->RowHeadersWidth = 51;
-			this->dataGridViewAdmin->RowTemplate->Height = 24;
-			this->dataGridViewAdmin->SelectionMode = System::Windows::Forms::DataGridViewSelectionMode::FullRowSelect;
-			this->dataGridViewAdmin->Size = System::Drawing::Size(1107, 613);
-			this->dataGridViewAdmin->TabIndex = 1;
+			this->dataGridViewRequest->AllowUserToAddRows = false;
+			this->dataGridViewRequest->AllowUserToDeleteRows = false;
+			this->dataGridViewRequest->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			this->dataGridViewRequest->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridViewRequest->Location = System::Drawing::Point(4, 17);
+			this->dataGridViewRequest->Margin = System::Windows::Forms::Padding(2);
+			this->dataGridViewRequest->Name = L"dataGridViewRequest";
+			this->dataGridViewRequest->ReadOnly = true;
+			this->dataGridViewRequest->RowHeadersWidth = 51;
+			this->dataGridViewRequest->RowTemplate->Height = 24;
+			this->dataGridViewRequest->SelectionMode = System::Windows::Forms::DataGridViewSelectionMode::FullRowSelect;
+			this->dataGridViewRequest->Size = System::Drawing::Size(908, 498);
+			this->dataGridViewRequest->TabIndex = 1;
 			// 
 			// menuStrip1
 			// 
@@ -121,7 +125,8 @@ namespace PharmacyDB {
 			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(1) { this->âûõîäToolStripMenuItem });
 			this->menuStrip1->Location = System::Drawing::Point(0, 0);
 			this->menuStrip1->Name = L"menuStrip1";
-			this->menuStrip1->Size = System::Drawing::Size(1582, 28);
+			this->menuStrip1->Padding = System::Windows::Forms::Padding(4, 2, 0, 2);
+			this->menuStrip1->Size = System::Drawing::Size(1188, 24);
 			this->menuStrip1->TabIndex = 4;
 			this->menuStrip1->Text = L"menuStrip1";
 			// 
@@ -132,100 +137,110 @@ namespace PharmacyDB {
 					this->âåğíóòüñÿÍàçàäToolStripMenuItem
 			});
 			this->âûõîäToolStripMenuItem->Name = L"âûõîäToolStripMenuItem";
-			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(67, 24);
+			this->âûõîäToolStripMenuItem->Size = System::Drawing::Size(53, 20);
 			this->âûõîäToolStripMenuItem->Text = L"Âûõîä";
 			// 
 			// âûéòèÈçÏğîãğàììûToolStripMenuItem
 			// 
 			this->âûéòèÈçÏğîãğàììûToolStripMenuItem->Name = L"âûéòèÈçÏğîãğàììûToolStripMenuItem";
-			this->âûéòèÈçÏğîãğàììûToolStripMenuItem->Size = System::Drawing::Size(243, 26);
+			this->âûéòèÈçÏğîãğàììûToolStripMenuItem->Size = System::Drawing::Size(193, 22);
 			this->âûéòèÈçÏğîãğàììûToolStripMenuItem->Text = L"Âûéòè èç ïğîãğàììû";
 			// 
 			// âåğíóòüñÿÍàçàäToolStripMenuItem
 			// 
 			this->âåğíóòüñÿÍàçàäToolStripMenuItem->Name = L"âåğíóòüñÿÍàçàäToolStripMenuItem";
-			this->âåğíóòüñÿÍàçàäToolStripMenuItem->Size = System::Drawing::Size(243, 26);
+			this->âåğíóòüñÿÍàçàäToolStripMenuItem->Size = System::Drawing::Size(193, 22);
 			this->âåğíóòüñÿÍàçàäToolStripMenuItem->Text = L"Âåğíóòüñÿ íàçàä";
 			// 
 			// maskedTextBoxPrice
 			// 
-			this->maskedTextBoxPrice->Location = System::Drawing::Point(6, 225);
+			this->maskedTextBoxPrice->Location = System::Drawing::Point(4, 183);
+			this->maskedTextBoxPrice->Margin = System::Windows::Forms::Padding(2);
 			this->maskedTextBoxPrice->Name = L"maskedTextBoxPrice";
-			this->maskedTextBoxPrice->Size = System::Drawing::Size(200, 22);
+			this->maskedTextBoxPrice->Size = System::Drawing::Size(151, 20);
 			this->maskedTextBoxPrice->TabIndex = 50;
 			// 
 			// label5
 			// 
 			this->label5->AutoSize = true;
-			this->label5->Location = System::Drawing::Point(3, 205);
+			this->label5->Location = System::Drawing::Point(2, 167);
+			this->label5->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label5->Name = L"label5";
-			this->label5->Size = System::Drawing::Size(47, 17);
+			this->label5->Size = System::Drawing::Size(36, 13);
 			this->label5->TabIndex = 51;
 			this->label5->Text = L"Öåíà:";
 			// 
 			// maskedTextBoxNumber1
 			// 
-			this->maskedTextBoxNumber1->Location = System::Drawing::Point(6, 180);
+			this->maskedTextBoxNumber1->Location = System::Drawing::Point(4, 146);
+			this->maskedTextBoxNumber1->Margin = System::Windows::Forms::Padding(2);
 			this->maskedTextBoxNumber1->Name = L"maskedTextBoxNumber1";
-			this->maskedTextBoxNumber1->Size = System::Drawing::Size(44, 22);
+			this->maskedTextBoxNumber1->Size = System::Drawing::Size(34, 20);
 			this->maskedTextBoxNumber1->TabIndex = 48;
 			// 
 			// label4
 			// 
 			this->label4->AutoSize = true;
-			this->label4->Location = System::Drawing::Point(3, 160);
+			this->label4->Location = System::Drawing::Point(2, 130);
+			this->label4->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label4->Name = L"label4";
-			this->label4->Size = System::Drawing::Size(105, 17);
+			this->label4->Size = System::Drawing::Size(82, 13);
 			this->label4->TabIndex = 49;
 			this->label4->Text = L"Íîìåğ àïòåêè:";
 			// 
 			// maskedTextBoxCountry
 			// 
-			this->maskedTextBoxCountry->Location = System::Drawing::Point(6, 90);
+			this->maskedTextBoxCountry->Location = System::Drawing::Point(4, 73);
+			this->maskedTextBoxCountry->Margin = System::Windows::Forms::Padding(2);
 			this->maskedTextBoxCountry->Name = L"maskedTextBoxCountry";
-			this->maskedTextBoxCountry->Size = System::Drawing::Size(200, 22);
+			this->maskedTextBoxCountry->Size = System::Drawing::Size(151, 20);
 			this->maskedTextBoxCountry->TabIndex = 46;
 			// 
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(3, 70);
+			this->label3->Location = System::Drawing::Point(2, 57);
+			this->label3->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label3->Name = L"label3";
-			this->label3->Size = System::Drawing::Size(165, 17);
+			this->label3->Size = System::Drawing::Size(126, 13);
 			this->label3->TabIndex = 47;
 			this->label3->Text = L"Ñòğàíà-ïğîèçâîäèòåëü:";
 			// 
 			// maskedTextBoxDate
 			// 
 			this->maskedTextBoxDate->BeepOnError = true;
-			this->maskedTextBoxDate->Location = System::Drawing::Point(6, 135);
+			this->maskedTextBoxDate->Location = System::Drawing::Point(4, 110);
+			this->maskedTextBoxDate->Margin = System::Windows::Forms::Padding(2);
 			this->maskedTextBoxDate->Mask = L"0000/00/00";
 			this->maskedTextBoxDate->Name = L"maskedTextBoxDate";
-			this->maskedTextBoxDate->Size = System::Drawing::Size(200, 22);
+			this->maskedTextBoxDate->Size = System::Drawing::Size(151, 20);
 			this->maskedTextBoxDate->TabIndex = 44;
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(3, 115);
+			this->label1->Location = System::Drawing::Point(2, 93);
+			this->label1->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(217, 17);
+			this->label1->Size = System::Drawing::Size(173, 13);
 			this->label1->TabIndex = 45;
 			this->label1->Text = L"Äàòà ïğîèçâîäñòâà (ãããã.ìì.ää):";
 			// 
 			// maskedTextBoxName
 			// 
-			this->maskedTextBoxName->Location = System::Drawing::Point(6, 45);
+			this->maskedTextBoxName->Location = System::Drawing::Point(4, 37);
+			this->maskedTextBoxName->Margin = System::Windows::Forms::Padding(2);
 			this->maskedTextBoxName->Name = L"maskedTextBoxName";
-			this->maskedTextBoxName->Size = System::Drawing::Size(200, 22);
+			this->maskedTextBoxName->Size = System::Drawing::Size(151, 20);
 			this->maskedTextBoxName->TabIndex = 42;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
-			this->label2->Location = System::Drawing::Point(3, 25);
+			this->label2->Location = System::Drawing::Point(2, 20);
+			this->label2->Margin = System::Windows::Forms::Padding(2, 0, 2, 0);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(76, 17);
+			this->label2->Size = System::Drawing::Size(60, 13);
 			this->label2->TabIndex = 43;
 			this->label2->Text = L"Íàçâàíèå:";
 			// 
@@ -241,30 +256,34 @@ namespace PharmacyDB {
 			this->groupBox2->Controls->Add(this->maskedTextBoxCountry);
 			this->groupBox2->Controls->Add(this->maskedTextBoxDate);
 			this->groupBox2->Controls->Add(this->label3);
-			this->groupBox2->Location = System::Drawing::Point(1187, 292);
+			this->groupBox2->Location = System::Drawing::Point(941, 28);
+			this->groupBox2->Margin = System::Windows::Forms::Padding(2);
 			this->groupBox2->Name = L"groupBox2";
-			this->groupBox2->Size = System::Drawing::Size(314, 300);
+			this->groupBox2->Padding = System::Windows::Forms::Padding(2);
+			this->groupBox2->Size = System::Drawing::Size(236, 244);
 			this->groupBox2->TabIndex = 52;
 			this->groupBox2->TabStop = false;
-			this->groupBox2->Text = L"groupBox2";
+			this->groupBox2->Text = L"Óñëîâèÿ ïîèñêà";
 			// 
 			// RequestForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::SkyBlue;
-			this->ClientSize = System::Drawing::Size(1582, 677);
+			this->ClientSize = System::Drawing::Size(1188, 557);
 			this->Controls->Add(this->groupBox2);
 			this->Controls->Add(this->menuStrip1);
 			this->Controls->Add(this->groupBox1);
+			this->Margin = System::Windows::Forms::Padding(2);
 			this->MaximizeBox = false;
-			this->MaximumSize = System::Drawing::Size(1600, 724);
-			this->MinimumSize = System::Drawing::Size(1600, 724);
+			this->MaximumSize = System::Drawing::Size(1204, 596);
+			this->MinimumSize = System::Drawing::Size(1204, 596);
 			this->Name = L"RequestForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"RequestForm";
+			this->Shown += gcnew System::EventHandler(this, &RequestForm::RequestForm_Shown);
 			this->groupBox1->ResumeLayout(false);
-			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewAdmin))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridViewRequest))->EndInit();
 			this->menuStrip1->ResumeLayout(false);
 			this->menuStrip1->PerformLayout();
 			this->groupBox2->ResumeLayout(false);
@@ -274,5 +293,8 @@ namespace PharmacyDB {
 
 		}
 #pragma endregion
-	};
+	private: void Header();//çàãîëîâîê òàáëèöû
+	private: void Show_meds(int _countData);//ïîêàçûâàåò äàííûå â äàòà ãğèä
+	private: System::Void RequestForm_Shown(System::Object^ sender, System::EventArgs^ e);
+};
 }
